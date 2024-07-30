5fb475927d330cf6ac35b9e20852d1a8db426f34
VietHieu2 <viethieu972@gmail.com>
2024-07-30 16:52:48 +0700

    commit-auth

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import LeftSideBar from "@/components/layout/LeftSideBar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "VietHieu - Admin Auth",
+  description: "Admin dashboard to manage VietHieu's data",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+            <div className="flex max-lg:flex-col">
+                <LeftSideBar />
+                <div className="flex-1">{children}</div>
+            </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
